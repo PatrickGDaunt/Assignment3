fde15997b095c2d1feb4a728745f2e34f9541864
PatrickGDaunt <patrickdaunt@icloud.com>
2020-07-28 21:10:24 -0400

    Initial commit v0.0. 28 2110 Z-4 July 2020

---
+++ Assignment3.py
@@ -0,0 +1,10 @@
+'''
+    Project name: Week 5 - SQL
+    File name: pythonRepos.py
+    Author: Patrick D
+    DTG created: 28 2104 Z-4 July 2020
+    DTG last modification: 28 2105 Z-4 July 2020
+    Python Version: 3.7.4
+    Description: Creating and editing databases with SQL
+'''
+
